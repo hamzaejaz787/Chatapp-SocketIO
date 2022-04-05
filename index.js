cef6e3e68c3108d2a5c91e981408f123b79cad47
hamzaejaz787 <hejaz389@gmail.com>
2022-04-05 15:57:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+    console.log(`Message: ${msg}`);
+  });
+});
+
+server.listen(3080, () => {
+  console.log("Listening on port 3080");
+});
